52eea84b38d2ed6cede96932e8efe11aa045e35b
Binggang Liu <binggangliu@Binggangs-MacBook-Pro.local>
2019-12-23 17:11:18 -0600

    Initial commit

---
+++ testcode.py
@@ -0,0 +1,26 @@
+"""
+# !/usr/bin/env python
+"""
+
+import math
+import sys
+from os import rename
+
+import requests
+
+
+print(sys.version)
+# print(sys.executable)
+def greet(who):
+    greeting = "Hello, {}!".format(who)
+    return greeting
+
+
+a = "Dave"
+print(greet(a))
+print(greet("John"))
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+
+# name = input("Your Name: ")
+# print("Hello, ", name)
